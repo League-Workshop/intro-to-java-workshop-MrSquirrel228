269f3933b1f9ecf831da78773c65e1bf93f786f2
lilacbrowne <league@WTS22.attlocal.net>
2018-01-03 14:50:09 -0800

    day 3

--- src/day3/StephenHawking.java
+++ src/day3/StephenHawking.java
@@ -1,32 +1,30 @@
 package day3;
 
 import javax.swing.JOptionPane;
-import com.sun.speech.freetts.Voice;
-import com.sun.speech.freetts.VoiceManager;
 
 public class StephenHawking {
-	
+
 	// 1. make a main method and put steps 2, 3 & 4 inside it
-	
-	// 2. ask the user for a sentence
-	
-	// 3. call the speak method below and send it the sentence
+	public static void main(String[] args) {
 
-	// 4. repeat steps 2 and 3 a lot of times
+		// 2. ask the user for a sentence
+		for (int i = 0; i < 5; i++) {
 
-	
-	
-	
-	
-	
-	
-	
-	/* Don’t change this…. */
-	static void speak(String words) {
-		Voice voice = VoiceManager.getInstance().getVoice("kevin16");
-		voice.allocate();
-		voice.speak(words);
-		voice.deallocate();
+			String sentance = JOptionPane.showInputDialog("Please type a sentance?");
+			speak(sentance);
+		}
+		// 3. call the speak method below and send it the sentence
+
+		// 4. repeat steps 2 and 3 a lot of times
+
 	}
 
+	/* Don’t change this…. */
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
