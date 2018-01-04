5720077715b5d1a247f84d5952434da0e24c36e5
clairebickett <league@WTS5.attlocal.net>
2018-01-04 14:27:55 -0800

    day4

--- src/day4/QuizGame.java
+++ src/day4/QuizGame.java
@@ -9,7 +9,7 @@ public class QuizGame {
 		// 2.  Ask the user a question 
 		
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if
 		// 4.  if the user's answer is correct
 		
 		// -- add one to their score 
